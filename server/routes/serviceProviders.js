a1574dfa1403013f71def96552cdb4e6c83a547d
RoyElia <roy2477@gmail.com>
2018-12-20 11:09:54 +0200

    Added api getAllServiceProviders

--- server/routes/serviceProviders.js
+++ server/routes/serviceProviders.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const {ServiceProviders} = require('../DBorm/DBorm');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource serviceProviders');
+    ServiceProviders.findAll()
+        .then(serviceProviders => {
+            console.log(serviceProviders);
+            res.status(200).send(serviceProviders);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send(err);
+        })
 });
 
 module.exports = router;
