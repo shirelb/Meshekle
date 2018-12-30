6a140fcd1ef62cd8c3b0e7b6edd4a297132fb73b
ShirelB <shirel1b@gmail.com>
2018-12-30 19:26:49 +0200

    end of login screen

--- app/src/screens/loginScreen/LoginScreen.js
+++ app/src/screens/loginScreen/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text,Image,View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import PropTypes from 'prop-types'
 import axios from 'axios';
 import styles from './LoginStyles';
@@ -7,6 +7,7 @@ import {SERVER_URL} from '../../shared/constants'
 import Button from "../../components/submitButton/Button";
 import FormTextInput from "../../components/formTextInput/FormTextInput";
 import strings from "../../shared/strings";
+
 const imageLogo = require("../../images/logo4000.png");
 
 export default class LoginScreen extends Component {
@@ -19,37 +20,75 @@ export default class LoginScreen extends Component {
     state = {
         userId: '',
         password: '',
-        err:null,
+        err: [],
+    };
+
+    validate = (userId, password) => {
+        console.log('in validate func');
+        const errors = [];
+
+        if (userId.length === 0) {
+            errors.push("הוסף ת.ז. ונסה להתחבר שוב");
+        } else {
+            if (userId.length > 9) {
+                errors.push("ת.ז. צריכה להיות באורך של מקסימום 9 ספרות");
+            }
+            if (!(/^\d+$/.test(userId))) {
+                errors.push("ת.ז. צריכה להכיל רק ספרות");
+            }
+        }
+
+
+        if (password.length === 0) {
+            errors.push("הוסף סיסמא ונסה להתחבר שוב");
+        } else {
+            if (password.length < 8) {
+                errors.push("סיסמא צריכה להכיל 8 ספרות לפחות");
+            }
+            if (password.length > 12) {
+                errors.push("סיסמא צריכה להכיל 12 ספרות לכל היותר");
+            }
+            if (!(/\d/.test(password) && /[a-zA-Z]/.test(password))) {
+                errors.push("סיסמא צריכה להכיל לפחות ספרה אחת ולפחות אות אחת");
+            }
+        }
+
+        return errors;
     };
 
     onSubmitPress = () => {
         console.log("in onSubmitPress");
-        return axios.post(`${SERVER_URL}/api/users/login/authenticate`,
-            {
-                "userId": this.state.userId,
-                "password": this.state.password
-            },
-        )
-            .then((response) => {
-                console.log("after get response");
-                console.log(response);
-                // deviceStorage.saveKey("id_token", response.data.jwt);
-                this.props.onLoginPress();
-            })
-            .catch((error) => {
-                console.log("after get error");
-                console.log(error);
-                this.setState({err: error})
-            });
+        const userId = this.state.userId;
+        const password = this.state.password;
+
+        const errors = this.validate(userId, password);
+        if (errors.length > 0) {
+            console.log(errors);
+            this.setState({err: errors});
+        } else {
+            axios.post(`${SERVER_URL}/api/users/login/authenticate`,
+                {
+                    "userId": this.state.userId,
+                    "password": this.state.password
+                },
+            )
+                .then((response) => {
+                    console.log(response);
+                    // deviceStorage.saveKey("id_token", response.data.jwt);
+                    this.props.onLoginPress();
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.setState({err: [error.response.data.message]});
+                });
+        }
     };
 
 
     render() {
-        return (
-            /*
-            <ScrollView style={styles.container}>
-*/
+        // const errors = this.state.err;
 
+        return (
             <View style={styles.container}>
                 <Image source={imageLogo} style={styles.logo}/>
                 <Text style={styles.titleText}>
@@ -58,12 +97,14 @@ export default class LoginScreen extends Component {
                 <View style={styles.form}>
                     <FormTextInput
                         value={this.state.userId}
-                        onChangeText={(userId) => this.setState({userId})}
+                        onChangeText={(userId) => this.setState({userId: userId})}
+                        onFocus={() => this.setState({err: []})}
                         placeholder={strings.USER_ID_PLACEHOLDER}
                     />
                     <FormTextInput
                         value={this.state.password}
-                        onChangeText={(password) => this.setState({password})}
+                        onChangeText={(password) => this.setState({password: password})}
+                        onFocus={() => this.setState({err: []})}
                         placeholder={strings.PASSWORD_PLACEHOLDER}
                         secureTextEntry={true}
                     />
@@ -71,34 +112,14 @@ export default class LoginScreen extends Component {
                         label={strings.LOGIN}
                         onPress={this.onSubmitPress.bind(this)}
                     />
-                    this.state.err ? <Text style={styles.errorText}>{this.state.err}</Text> : null
-                    {/*<Text style={styles.errorText}>
-                        {this.state.err}
-                    </Text>*/}
-                    {/* <Text style={styles.loginLabel}>
-                    התחבר
-                </Text>*/}
-                    {/*<TextInput
-                    value={this.state.userId}
-                    onChangeText={(userId) => this.setState({ userId })}
-                    placeholder={'ת.ז.'}
-                    style={styles.input}
-                />*/}
-                    {/*<TextInput
-                    value={this.state.password}
-                    onChangeText={(password) => this.setState({ password })}
-                    placeholder={'סיסמא'}
-                    secureTextEntry={true}
-                    style={styles.input}
-                />*/}
-                    {/*
-                <View style={{margin: 7}}/>
-*/}
-                    {/* <Button
-                    onPress={this.onSubmitPress.bind(this)}
-                    title="שלח"
-                    style={styles.submitBtn}
-                />*/}
+                    {this.state.err.map(error => (
+                        <View key={error}>
+                            <Text style={styles.errorText}>
+                                {/*שגיאה: */}
+                                {error}
+                            </Text>
+                        </View>
+                    ))}
                 </View>
             </View>
         )
