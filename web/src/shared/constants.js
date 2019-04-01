ddc661995f0f61e17b7dd16c4604dee33a1bf510
ShirelB <shirel1b@gmail.com>
2019-04-01 15:27:39 +0300

    adding disconnect to real time

--- web/src/shared/constants.js
+++ web/src/shared/constants.js
@@ -6,14 +6,15 @@ export const SERVER_URL = __DEV__ ?
 */
 
 // import socketIOClient from "socket.io-client";
-import openSocket from "socket.io-client";
+import io from "socket.io-client";
 import store from "store";
 
 const SERVER_URL = "http://192.168.0.104:4000";
 
-const WEB_SOCKET = openSocket(SERVER_URL);
+const WEB_SOCKET = io(SERVER_URL);
 
 var connectToServerSocket = (serviceProviderId) => {
+    WEB_SOCKET.connect();
     WEB_SOCKET.on('socketServerID', function (socketServerID) {
         console.log('Connection to server established. SocketID is', socketServerID);
         WEB_SOCKET.emit('storeWebClientInfo', {serviceProviderId: serviceProviderId});
