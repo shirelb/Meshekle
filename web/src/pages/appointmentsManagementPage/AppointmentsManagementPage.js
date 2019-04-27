42b58dd83cefd52119eee5330cbde7350bf0c900
shirelb <shirel.brandes>
2019-04-27 23:25:44 +0300

    app- appointments- fixed bug- show day selected info when pressing day on calendar web- appointments- add try catch to background color (hex2rgba)

--- web/src/pages/appointmentsManagementPage/AppointmentsManagementPage.js
+++ web/src/pages/appointmentsManagementPage/AppointmentsManagementPage.js
@@ -30,8 +30,14 @@ const colorEventByRole = {
 
 
 const hex2rgba = (hex, alpha = 1) => {
-    const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
-    return `rgba(${r},${g},${b},${alpha})`;
+    try{
+        const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+        return `rgba(${r},${g},${b},${alpha})`;
+    }
+    catch (e) {
+        return hex;
+    }
+
 };
 
 class AppointmentsManagementPage extends React.Component {
