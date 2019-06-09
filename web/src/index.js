c27b00312dbee090be202e95c8e0a008bbf01418
shirelb <shirel.brandes>
2019-06-09 21:31:13 +0300

    web - logging error handling

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({dsn: "https://16dd618fe8ab4102bf7fd6929f87434a@sentry.io/1478107"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
